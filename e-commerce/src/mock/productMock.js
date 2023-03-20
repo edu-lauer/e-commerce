8667a5e15cc22b381f308a837d76c8a275a8acd7
edu-lauer <lauer.edu@gmail.com>
2023-03-20 16:19:55 -0300

    Criado o arquivo productMock.js e adicionado as imagens

---
+++ e-commerce/src/mock/productMock.js
@@ -0,0 +1,62 @@
+export const PRODUCTS_MOCK = [
+    {
+        id: 1,
+        imgPath: "xbox-series.svg",
+        imgPathDetail: "../../public/xbox-series.svg",
+        title: "Microsoft Xbox Series X | Preto",
+        description: "O aparelho é sucessor do Xbox One e Xbox One X, e pode ser considerado um dos videogames mais poderosos do mundo, com capacidade de reproduzir jogos em 4K a 120 FPS.",
+        price: "R$ 2.000,00",
+        colors: ["black"],
+
+    },
+
+    {
+        id: 2,
+        imgPath: "xbox-series-s.svg",
+        imgPathDetail: "../../public/xbox-series-s.svg",
+        title: "Microsoft Xbox Series S | Branco",
+        description: "O Xbox Series S conta com suporte à resolução 1440p, taxa de quadros de até 120 fps e possui upscaling para 4K em jogos",
+        price: "R$ 2.899,00",
+        colors: ["black", "white"],
+    },
+
+    {
+        id: 3,
+        imgPath: "nintendo-switch.svg",
+        imgPathDetail: "../../public/nintendo-switch.svg",
+        title: "Nintendo Switch com Jogo",
+        description: "O Nintendo Switch é um console de videogame híbrido, o sistema principal contém o tablet \"Switch Console\", o módulo 'Switch Dock' e os dois controles \"Joy-Con\" (direito e esquerdo), os elásticos chamados de \"Joy-Con Strap\", o suporte gamepad chamado de \"Joy-Con Grip\", o cabo de energia e o cabo HDMI.",
+        price: "R$ 1.790,00",
+        colors: ["white","red", "black"],
+    },
+
+    {
+        id: 4,
+        imgPath: "god-of-war.svg",
+        imgPathDetail: "../../public/god-of-war.svg",
+        title: "Jogo God of War Playstation",
+        description: "Baseada em distintas mitologias, a história segue Kratos, um guerreiro espartano que foi levado a matar sua família por seu antigo mestre, o deus da guerra Ares",
+        price: "R$ 189,90",
+        colors: [],
+    },
+
+    {
+        id: 5,
+        imgPath: "uncharted.svg",
+        imgPathDetail: "../../public/uncharted.svg",
+        title: "Jogo Uncharted: The Nathan Drake Collection",
+        description: "Baseado em uma das séries de videogame mais vendidas e aclamadas pela crítica de todos os tempos, Uncharted: Fora do Mapa serve como uma prequela para os jogos, seguindo o jovem explorador Nathan \"Nate\" Drake (Tom Holland), descendente do grande explorador Francis Drake, antes de ser conhecido como um famoso explorador",
+        price: "R$ 220,00",
+        colors: [],
+    },
+
+    {
+        id: 6,
+        imgPath: "gran-turismo.svg",
+        imgPathDetail: "../../public/gran-turismo.svg",
+        title: "Jogo Gran Turismo",
+        description: "O Gran Turismo Sport conta com a mais moderna simulação de física veicular, o que inclui conhecimentos obtidos tanto no mundo real quanto virtual. Uma simulação física \"fácil de dirigir porque é realista\" foi alcançada",
+        price: "R$ 159,90",
+        colors: [],
+    }
+]
