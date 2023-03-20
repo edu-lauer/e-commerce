1b3a9aa79ccc6c9df00fd01df12a18a4539d03a4
edu-lauer <lauer.edu@gmail.com>
2023-03-20 16:38:59 -0300

    feito html renderizado no componente Header

--- e-commerce/src/components/Header/Header.jsx
+++ e-commerce/src/components/Header/Header.jsx
@@ -1,1 +1,11 @@
 import "./header.scss"
+
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>Header</div>
+    )
+}
+
+export default Header
--- e-commerce/src/views/Login/Login.jsx
+++ e-commerce/src/views/Login/Login.jsx
@@ -1,15 +1,27 @@
 import "./login.scss"
 import React from 'react'
 import { Link } from "react-router-dom"
+import Header from '../../components/Header/Header'
 
 const Login = () => {
     return (
-        <div>
-            <h1>Login</h1>
-            <button type='submit'>
-                <Link to={"/home"}>Entrar</Link>
-            </button>
-        </div>
+        <section className="login">
+            <Header />
+            <section className="login__form">
+                <h1></h1>
+                <h2></h2>
+                <div className="login__input-form">
+                    <label htmlFor="cpf">Digite seu CPF:</label>
+                    <input type="text" placeholder="cpf completo" id="cpf" className="login__input-cpf"></input>
+                    <label htmlFor="password">Digite seu CPF:</label>
+                    <input type="password" placeholder="********" id="password" className="login__input-password"></input>
+                </div>
+                <button type='submit'>
+                    <Link to={"/home"}>Entrar</Link>
+                </button>
+
+            </section>
+        </section>
     )
 }
 
