992781601c65582dda51b998fb6679d554110791
edu-lauer <lauer.edu@gmail.com>
2023-03-21 18:43:53 -0300

    Iniciado o componente card e seu scss

--- e-commerce/src/components/Card/Card.jsx
+++ e-commerce/src/components/Card/Card.jsx
@@ -1,1 +1,16 @@
 import "./card.scss"
+import React from 'react'
+
+const Card = ({ data }) => {
+    return (
+        <section className="card">
+            <div>
+                <img src={data.imgPath} alt={data.title} />
+                <p>{data.title}</p>
+                <button>Ver mais</button>
+            </div>
+        </section>
+    )
+}
+
+export default Card
