f8e504112b68807d28a96eee6c64dc616cac6539
edu-lauer <lauer.edu@gmail.com>
2023-03-21 15:59:43 -0300

    Header de login passou a ser um html incorporado e não mais um componente

--- e-commerce/src/views/Home/Home.jsx
+++ e-commerce/src/views/Home/Home.jsx
@@ -1,15 +1,19 @@
 import "./home.scss"
 import React from 'react'
 import { Link } from "react-router-dom"
+import Header from '../../components/Header/Header'
+import Card from '../../components/Card/Card'
+import mainImg from '../../assets/main-image.svg'
 
 const Home = () => {
     return (
-        <div>
-            <h1>Home</h1>
-            <button type='submit'>
-                <Link to={"/productdetail"}>Product Detail</Link>
-            </button>
-        </div>
+        <section className="home">
+            <Header />
+            <img className="home__main-img" src={ mainImg } alt="Imagem propagando do jogo, forbidden west, de playstation 4." />
+            <div>
+                <Card />
+            </div>
+        </section>
     )
 }
 
