b9e7440a0239d7535103e613ad4da6304ca8f59a
edu-lauer <lauer.edu@gmail.com>
2023-03-21 15:03:42 -0300

    Componente Header finalizado

--- e-commerce/src/components/Header/Header.jsx
+++ e-commerce/src/components/Header/Header.jsx
@@ -1,10 +1,13 @@
 import "./header.scss"
+import logoDnc from "../../assets/dnc-logo.svg"
 
 import React from 'react'
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <section className="header">
+            <img src={logoDnc} alt="logo da escola dnc" />
+        </section>
     )
 }
 
