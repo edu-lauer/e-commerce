1baedd373165d3cab88074bf46021f1d4d39b473
edu-lauer <lauer.edu@gmail.com>
2023-03-21 14:47:50 -0300

    Alterado valor 2° label para "senha"

--- e-commerce/src/views/Login/Login.jsx
+++ e-commerce/src/views/Login/Login.jsx
@@ -8,12 +8,12 @@ const Login = () => {
         <section className="login">
             <Header />
             <section className="login__form">
-                <h1></h1>
-                <h2></h2>
+                <h1>Acesse com seu login ou cadastre-se!</h1>
+                <h2>você pode entrar com o seu CPF</h2>
                 <div className="login__input-form">
                     <label htmlFor="cpf">Digite seu CPF:</label>
                     <input type="text" placeholder="cpf completo" id="cpf" className="login__input-cpf"></input>
-                    <label htmlFor="password">Digite seu CPF:</label>
+                    <label htmlFor="password">Senha:</label>
                     <input type="password" placeholder="********" id="password" className="login__input-password"></input>
                 </div>
                 <button type='submit'>
