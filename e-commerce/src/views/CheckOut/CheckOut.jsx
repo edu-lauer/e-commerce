9aeb77b841a624a1973214fb5f444b27abcad098
edu-lauer <lauer.edu@gmail.com>
2023-03-26 10:00:46 -0300

    View check-out feiita.

--- e-commerce/src/views/CheckOut/CheckOut.jsx
+++ e-commerce/src/views/CheckOut/CheckOut.jsx
@@ -1,14 +1,33 @@
 import "./checkOut.scss"
 import React from 'react'
+import Header from '../../components/Header/Header'
+import Footer from '../../components/Footer/Footer'
 import { Link } from "react-router-dom"
+import { useParams } from 'react-router-dom'
 
-const CheckOut = () => {
+const CheckOut = ({ data }) => {
+
+    const { checkOutId } = useParams()
+    const selectedProduct = data.find((product) => product.id == checkOutId)
+
     return (
-        <div>
-            <h1>Check Out</h1>
-            <button type='submit'>
-                <Link to={"/"}>Volta para Login</Link>
-            </button>
+        <div className="check-out">
+            <Header />
+            <h1>Finalizar compras</h1>
+            <form className="check-out__form">
+                <label htmlFor="" className="check-out__form-label">Digite seu nome:</label>
+                <input type="text" className="check-out__form-input" placeholder="Nome completo"/>
+                <label htmlFor="" className="check-out__form-label">Digite seu CPF:</label>
+                <input type="text" className="check-out__form-input" placeholder="XXX.XXX.XXX-XX"/>
+                <label htmlFor="" className="check-out__form-label">Endereço:</label>
+                <input type="text" className="check-out__form-input" placeholder="Endereço residencial"/>
+                <label htmlFor="" className="check-out__form-label">Forma de Pagamento:</label>
+                <input type="text" className="check-out__form-input" placeholder="Escolha forma de pagamento"/>
+                <button type='submit'>
+                    <Link to={"/"}>Confirmar Pedido</Link>
+                </button>
+            </form>
+            <Footer />  
         </div>
     )
 }
